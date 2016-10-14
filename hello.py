cfaf1e9d108f03771d8aafadc368088fefd75fc0
magic <zhongxy@hotmail.com>
2016-10-14 14:49:31 +0800

    3e

--- hello.py
+++ hello.py
@@ -1,12 +1,16 @@
 from flask import Flask,render_template
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',current_time=datetime.utcnow())
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
